d1baa957283c14b6b17d8c33f6972abc8a3b18fd
hackorama <kishan.thomas@gmail.com>
2019-02-03 07:30:24 -0800

    Adding option to use external database connection

--- src/main/java/com/hackorama/flags/Main.java
+++ src/main/java/com/hackorama/flags/Main.java
@@ -26,8 +26,13 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         usage(args);
         if (Util.usingJDBC(args)) {
-            new FlagService().configureUsing(new SpringServer("Flag Service Using H2 JDBC Store"))
-                    .configureUsing(new JDBCDataStore()).start();
+            if (args.length >= 5) { // Connect to external RDBMS database
+                new FlagService().configureUsing(new SpringServer("Flag Service Using MySQL JDBC Store"))
+                        .configureUsing(new JDBCDataStore(args[1], args[2], args[3], args[4])).start();
+            } else { // Use default embedded RDBMS database
+                new FlagService().configureUsing(new SpringServer("Flag Service Using H2 JDBC Store"))
+                        .configureUsing(new JDBCDataStore()).start();
+            }
         } else if (Util.usingMapDB(args)) {
             new FlagService().configureUsing(new SpringServer("Flag Service Using MapDB Key Value Store"))
                     .configureUsing(new MapdbDataStore()).start();
@@ -40,12 +45,18 @@ public class Main {
     private static void usage(String[] args) {
         if (args.length > 0 && ("-h".equals(args[0]) || "--h".equals(args[0]) || "-help".equals(args[0])
                 || "--help".equals(args[0]) || "help".equals(args[0]))) {
+            System.out.println("");
             System.out.println("Flag Service");
             System.out.println("");
             System.out.println("Usage : java Main [-jdbc] [-mapdb] [-h]");
-            System.out.println("   -jdbc  Use H2 JDBC Data Store");
-            System.out.println("   -mapdb Use Mapdb Key Value Data Store");
-            System.out.println("   -h     Print this help");
+            System.out.println("   -jdbc");
+            System.out.println("         Use the default embedded H2 JDBC Data Store");
+            System.out.println("   -jdbc [url] [driver] [user] [pass]");
+            System.out.println("         Connect to a JDBC Data Store like MySQL");
+            System.out.println("   -mapdb");
+            System.out.println("         Use Mapdb Key Value Data Store");
+            System.out.println("   -h");
+            System.out.println("         Print this help");
             System.exit(0);
         }
     }
